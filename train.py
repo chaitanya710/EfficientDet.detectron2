4e555d7ec9aaa992efca8ef23d9519dc781b3d44
Chaitanya Gadgil <60541234+chaitanya710@users.noreply.github.com>
2022-12-25 23:58:05 +0530

    Update train.py

--- train.py
+++ train.py
@@ -32,6 +32,7 @@ from detectron2.data import (
     build_detection_test_loader,
     build_detection_train_loader,
 )
+from detectron2.data.datasets.coco import load_coco_json, register_coco_instances
 from detectron2.engine import default_argument_parser, default_setup, launch
 from detectron2.evaluation import (
 
